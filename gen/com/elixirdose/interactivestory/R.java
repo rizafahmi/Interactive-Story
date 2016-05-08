a27962f513a5f7393e052e683106bd0b4399837e
Riza Fahmi <rizafahmi@gmail.com>
2016-05-08 16:40:13 +0700

    Add asset images

--- gen/com/elixirdose/interactivestory/R.java
+++ gen/com/elixirdose/interactivestory/R.java
@@ -23,6 +23,14 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int main_title=0x7f020001;
+        public static final int page0=0x7f020002;
+        public static final int page1=0x7f020003;
+        public static final int page2=0x7f020004;
+        public static final int page3=0x7f020005;
+        public static final int page4=0x7f020006;
+        public static final int page5=0x7f020007;
+        public static final int page6=0x7f020008;
     }
     public static final class id {
         public static final int action_settings=0x7f080000;
