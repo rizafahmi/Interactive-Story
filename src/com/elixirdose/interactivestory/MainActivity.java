80e9e722a35039c5e4a07669b7d0631d24f0f74a
Riza Fahmi <rizafahmi@gmail.com>
2016-05-08 19:40:19 +0700

    Add onclick listener di button

--- src/com/elixirdose/interactivestory/MainActivity.java
+++ src/com/elixirdose/interactivestory/MainActivity.java
@@ -1,34 +1,38 @@
 package com.elixirdose.interactivestory;
 
 import android.app.Activity;
+import android.net.wifi.p2p.WifiP2pManager.ActionListener;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	private EditText mNameEditText;
+	private Button mStartButton;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+		
+		mNameEditText = (EditText) findViewById(R.id.nameEditText);
+		mStartButton = (Button) findViewById(R.id.startButton);
+		
+		OnClickListener mOnClickListener = new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				String name = mNameEditText.getEditableText().toString();
+				Toast.makeText(MainActivity.this, name, Toast.LENGTH_LONG).show();
+			}
+		};
+		
+		mStartButton.setOnClickListener(mOnClickListener);
 	}
 }
