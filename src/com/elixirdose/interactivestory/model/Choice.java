219e63d7552ff84b42dcd9029d73b94568926e29
Riza Fahmi <rizafahmi@gmail.com>
2016-05-14 20:31:10 +0700

    Add setter and getter

--- src/com/elixirdose/interactivestory/model/Choice.java
+++ src/com/elixirdose/interactivestory/model/Choice.java
@@ -3,4 +3,16 @@ package com.elixirdose.interactivestory.model;
 public class Choice {
 	private String mText;
 	private int mNextPage;
+	public String getmText() {
+		return mText;
+	}
+	public void setmText(String mText) {
+		this.mText = mText;
+	}
+	public int getmNextPage() {
+		return mNextPage;
+	}
+	public void setmNextPage(int mNextPage) {
+		this.mNextPage = mNextPage;
+	}
 }
--- src/com/elixirdose/interactivestory/model/Page.java
+++ src/com/elixirdose/interactivestory/model/Page.java
@@ -5,5 +5,31 @@ public class Page {
 	private String mText;
 	private Choice mChoice1;
 	private Choice mChoice2;
+	
+	public int getmImageId() {
+		return mImageId;
+	}
+	public void setmImageId(int mImageId) {
+		this.mImageId = mImageId;
+	}
+	public String getmText() {
+		return mText;
+	}
+	public void setmText(String mText) {
+		this.mText = mText;
+	}
+	public Choice getmChoice1() {
+		return mChoice1;
+	}
+	public void setmChoice1(Choice mChoice1) {
+		this.mChoice1 = mChoice1;
+	}
+	public Choice getmChoice2() {
+		return mChoice2;
+	}
+	public void setmChoice2(Choice mChoice2) {
+		this.mChoice2 = mChoice2;
+	}
+	
 
 }
