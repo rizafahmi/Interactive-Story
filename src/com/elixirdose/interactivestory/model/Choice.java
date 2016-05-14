ac4d8bdeb2fae70b207658d73b3335793853f0d9
Riza Fahmi <rizafahmi@gmail.com>
2016-05-14 20:30:09 +0700

    Add two models: Page and Choice

---
+++ src/com/elixirdose/interactivestory/model/Choice.java
@@ -0,0 +1,6 @@
+package com.elixirdose.interactivestory.model;
+
+public class Choice {
+	private String mText;
+	private int mNextPage;
+}
---
+++ src/com/elixirdose/interactivestory/model/Page.java
@@ -0,0 +1,9 @@
+package com.elixirdose.interactivestory.model;
+
+public class Page {
+	private int mImageId;
+	private String mText;
+	private Choice mChoice1;
+	private Choice mChoice2;
+
+}
